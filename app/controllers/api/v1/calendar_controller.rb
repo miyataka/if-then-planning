2606171a75f474400e345153968810698acf1aeb
Takayuki Miyahara <voyager.3taka28@gmail.com>
2018-09-02 16:52:17 +0900

    add concern

---
+++ app/controllers/api/v1/calendar_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::CalendarController < ApplicationController
+    include GoogleCalendarAuthorize
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
---
+++ app/controllers/concerns/google_calendar_authorize.rb
@@ -0,0 +1,39 @@
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
+APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'.freeze
+CREDENTIALS_PATH = '../../client_secret.json'.freeze
+TOKEN_PATH = '../../token.yaml'.freeze
+SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+
+module GoogleCalendarAuthorize
+    extend ActiveSupport::Concern
+
+    def authorize
+      client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
+      token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
+      authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
+      user_id = 'default'
+      credentials = authorizer.get_credentials(user_id)
+      # TODO rewrite this logic
+      if credentials.nil?
+        url = authorizer.get_authorization_url(base_url: OOB_URI)
+        puts 'Open the following URL in the browser and enter the ' \
+             "resulting code after authorization:\n" + url
+        code = gets
+        credentials = authorizer.get_and_store_credentials_from_code(
+          user_id: user_id, code: code, base_url: OOB_URI
+        )
+      end
+      credentials
+    end
+
+    def initialize
+        service = Google::Apis::CalendarV3::CalendarService.new
+        service.client_options.application_name = APPLICATION_NAME
+        service.authorization = authorize
+    end
+end
