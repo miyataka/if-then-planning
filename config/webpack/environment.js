b07c0ac4f530b4a55ae0f1c738e8f7a93546e02c
Takayuki Miyahara <voyager.3taka28@gmail.com>
2018-09-09 13:02:24 +0900

    getting started bootstrap-vue again

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const css =  require('./loaders/css')
+const image =  require('./loaders/image')
 
 environment.loaders.append('vue', vue)
+environment.loaders.append('css', css)
+environment.loaders.append('image', image)
 module.exports = environment
